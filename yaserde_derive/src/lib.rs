afd969d4a5b05c43777bf750a7113716b49d789b
Ethan Dickson <ethanndickson@gmail.com>
2023-09-14 14:29:59 +1000

    feat: deafult yaserde derive

--- yaserde_derive/src/lib.rs
+++ yaserde_derive/src/lib.rs
@@ -31,6 +31,7 @@ pub fn derive_serialize(input: TokenStream) -> TokenStream {
   }
 }
 
+// Serialize & Deserialize a struct using it's UpperHex implementation
 #[proc_macro_derive(HexBinaryYaSerde)]
 pub fn derive_hexbinary(input: TokenStream) -> TokenStream {
   let serde: TokenStream2 = hexbinary_serde(input.clone()).into();
@@ -43,6 +44,7 @@ pub fn derive_hexbinary(input: TokenStream) -> TokenStream {
   .into()
 }
 
+// Serialize & Deserialize a primitive newtype by generating a FromStr & Display implementation
 #[proc_macro_derive(PrimitiveYaSerde)]
 pub fn derive_primitive(input: TokenStream) -> TokenStream {
   let serde: TokenStream2 = primitive_serde(input.clone()).into();
@@ -53,4 +55,10 @@ pub fn derive_primitive(input: TokenStream) -> TokenStream {
     #yaserde
   }
   .into()
+}
+
+// Serialize & Deserialize a type using it's existing FromStr & Display implementation
+#[proc_macro_derive(DefaultYaSerde)]
+pub fn derive_default(input: TokenStream) -> TokenStream {
+  primitive_yaserde(input).into()
 }
