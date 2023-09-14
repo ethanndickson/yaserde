7a211da2310c4c63fae9e639a948fa7522cac1fe
Ethan Dickson <ethanndickson@gmail.com>
2023-09-14 13:32:46 +1000

    feat: proc macro for printing bitflags as hex

--- yaserde_derive/src/lib.rs
+++ yaserde_derive/src/lib.rs
@@ -8,6 +8,8 @@ mod de;
 mod ser;
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(YaDeserialize, attributes(yaserde))]
 pub fn derive_deserialize(input: TokenStream) -> TokenStream {
@@ -25,4 +27,25 @@ pub fn derive_serialize(input: TokenStream) -> TokenStream {
     Ok(expanded) => expanded.into(),
     Err(msg) => panic!("{}", msg),
   }
+}
+
+#[proc_macro_derive(HexBinarySerde)]
+pub fn derive_hexbinary(input: TokenStream) -> TokenStream {
+  let DeriveInput { ident, .. } = parse_macro_input!(input);
+  quote! {
+    impl std::fmt::Display for #ident {
+      fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "0x{:X}", self.0)
+      }
+    }
+
+    impl ::std::str::FromStr for #ident {
+      type Err = ::std::string::String;
+
+      fn from_str(s: &::std::primitive::str) -> ::std::result::Result<Self, Self::Err> {
+        bitflags::parser::from_str(s).map_err(|e| e.to_string())
+      }
+    }
+  }
+  .into()
 }
